c30b4a45fb28c39d6f9dbb300ffb5a3aeebf791c
Kior0613e <ricardochicas95@gmail.com>
2020-02-15 09:51:42 -0600

    fix css styles

--- js/animations.js
+++ js/animations.js
@@ -1,56 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  // Scroll Elementos Menu
+  var home = $("#home").offset().top,
+    aboutUs = $("#about-us").offset().top,
+    contactUs = $("#contact-us").offset().top;
 
-// Scroll Elementos Menu
-	var home = $('#home').offset().top,
-		aboutUs = $('#about-us').offset().top,
-		contactUs = $('#contact-us').offset().top;
+  // Boton Home
+  $("#btn-home").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: 380
+      },
+      500
+    );
+  });
 
-// Boton Home
-	$('#btn-home').on('click', function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: 380
-		}, 500);
-	});
+  $("#btn-about-us").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: aboutUs - 40
+    });
+  });
 
-	$('#btn-about-us').on('click', function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: aboutUs - 40
-		});
-	});
-
-	$('#btn-contact-us').on('click', function(e){
-		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: contactUs - 40
-		});
-	});
-
-
-// Efecto Menu
-	/*$('.menu a').each(function(index, elemento){
-		$(this).css({
-			'top': '-200px'
-		});
-
-		$(this).animate({
-			top: '0'
-		},2000 + (index * 500));
-	}); */
-
-
-// Efecto Header
-	/*if ($(window).width() > 800){
-		$('header .header-text').css({
-			opacity: 0,
-			marginTop: 0
-		});
-
-		$('header .header-text').animate({
-			opacity: 1,
-			marginTop: '-52px'
-		},1500);
-		}
-*/
-}); 
+  $("#btn-contact-us").on("click", function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: contactUs - 40
+    });
+  });
+});
